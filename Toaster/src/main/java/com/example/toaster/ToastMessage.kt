e3d653d6b6d3e6838882cd2bca5421db5ed0fad2
Ankit Mourya <amourya@allot.com>
2024-01-17 22:38:16 +0530

    init

--- Toaster/src/main/java/com/example/toaster/ToastMessage.kt
+++ Toaster/src/main/java/com/example/toaster/ToastMessage.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.widget.Toast
 
 
-class ToastMessage {
-    fun ankitPleaseShowTheToastMessage(c: Context?, message: String?) {
+public class ToastMessage {
+     fun ankitPleaseShowTheToastMessage(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
 }
